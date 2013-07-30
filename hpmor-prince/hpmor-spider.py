fbab71e19a1ab8dca1a7ca3172eed36a3b69b1a1
acbart <acbart@vt.edu>
2013-07-29 22:43:57 -0400

    Vast improvements to hpmor-spidey, including argument parsing and caching

--- hpmor-prince/hpmor-spider.py
+++ hpmor-prince/hpmor-spider.py
@@ -1,15 +1,71 @@
 #!/usr/bin/python
 import sys, time, os, random, re, urllib
+import argparse
+import requests
+from bs4 import BeautifulSoup
+def removeNonAscii(s): return "".join(filter(lambda x: ord(x)<128, s))
 
-start = 1
-end = 0
-suffix = ""
-if len(sys.argv) > 1:
-	start = int(sys.argv[1])
-if len(sys.argv) > 2:
-	end = int(sys.argv[2])
-	suffix = "-"+str(start)+"-"+str(end)
-			
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--volumes", help="A comma separated list of the chapters to break on (e.g. 1,6,10,42).")
+parser.add_argument("-c", "--cached", help="If present, we will use the locally available files only.", action="store_true")
+args = parser.parse_args()
+
+def get_latest_chapter():
+    hpmor_content = requests.get("http://www.hpmor.com").content
+    hpmor_content_cleaned = removeNonAscii(hpmor_content).encode('ascii', 'ignore')
+    hpmor = BeautifulSoup(hpmor_content_cleaned)
+    latest_chapter_url = hpmor.find(id = "latest-chapter").find("a")["href"]
+    return int(re.search("(\d+)", latest_chapter_url).group(0))
+
+def get_chapter_content(chapter):
+    hpmor_content = requests.get("http://www.hpmor.com/chapter/"+str(chapter)).content
+    hpmor = BeautifulSoup(hpmor_content)
+    return hpmor
+
+def cache_file(id, raw):
+    with open("cache/{}.html".format(id), 'w') as file:
+        file.write(raw.prettify().encode('utf8'))
+        
+def load_file(path):
+    file = open(path, 'r')
+    raw = file.read().decode('utf8')
+    file.close()
+    return raw
+    
+def load_cached_file(id):
+    return load_file("cache/{}.html".format(id))
+    
+def parse_chapter(raw):
+    """
+    consumes raw html of page
+    returns title, body
+    """
+    return raw.find(id = "chapter-title"), raw.find(id = "storycontent")
+    
+first_chapter = 1
+last_chapter = get_latest_chapter()
+
+if args.volumes is None:
+    books = [(first_chapter, last_chapter)]
+else:
+    chapters = [int(chapter.strip()) for chapter in args.volumes.split(",")]
+    books = [(start, finish-1) for start, finish in zip(chapters, chapters[1:]+[1+last_chapter])]
+
+if not args.cached:
+    print "Downloading",
+    for chapter_id in xrange(first_chapter, 1+last_chapter):
+        print chapter_id,
+        cache_file(chapter_id, *get_chapter_content(chapter_id))
+        
+header = load_file("hpmor-header.html")
+footer = load_file("hpmor-footer.html")
+        
+for start, finish in books:
+    with open("", "w") as file:
+        
+    
+sys.exit()
+
 f = open('hpmor'+suffix+'.html', 'w')
 header = open('hpmor-header.html', 'r')
 f.write(header.read())
@@ -42,3 +98,8 @@ footer = open('hpmor-footer.html', 'r')
 f.write(footer.read())
 
 print 'done'
+
+
+#import subprocess
+
+#subprocess.call(["prince","foo.xml","bar.pdf"]);
